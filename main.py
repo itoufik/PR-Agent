7f0398d7155ead22eba6f44f2c9673b7377906d0
“Toufik <itoufik568@gmail.com>
2025-07-03 19:39:59 +0530

    Deleted spare files

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from pr-agent-project!")
-
-
-if __name__ == "__main__":
-    main()
--- test1.py
+++
@@ -1,7 +0,0 @@
-print("Hello World")
-print("This is a new line")
-import os
-print(os.getcwd())
-
-from pathlib import Path
-print(str(Path(__file__).parent))
